be67e04cbba9b2cbae68c0dee84f3c39afda5f54
Nour Lababidi <lababidinour4@gmail.com>
2026-08-17 18:56:38 +0300

    first commit

--- app/Filament/Resources/Businesses/BusinessResource.php
+++ app/Filament/Resources/Businesses/BusinessResource.php
@@ -28,7 +28,7 @@ class BusinessResource extends Resource
 
     protected static string|BackedEnum|null $navigationIcon = 'heroicon-o-building-storefront';
 
-    protected static ?string $navigationLabel = 'busie+ness';
+    protected static ?string $navigationLabel = 'Businesses';
 
     protected static ?string $modelLabel = 'Business';
 
